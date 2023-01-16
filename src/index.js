ac0e7100a3f4007be6c54351b29691d9ba66b4ae
Kender ROMAIN <kenderromain@gmail.com>
2023-01-16 17:51:16 -0400

    Add fontawesome dependencies in order to use them

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { displayTodos } from "./js/todo";
+import { displayTodos } from './js/todo';
+import './css/index.css';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faArrowLeft, faRefresh } from '@fortawesome/free-solid-svg-icons';
+
+// Using of the icon we want from fontawesome
+library.add(faRefresh, faArrowLeft);
+dom.watch();
 
 displayTodos();
