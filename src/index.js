e97c5d37f2f6f75f5ba6c6aeb7aa5368d7e95b0c
Kender ROMAIN <kenderromain@gmail.com>
2023-01-16 20:18:34 -0400

    Create addTodo function.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faArrowLeft, faRefresh } from '@fortawesome/free-solid-svg-icons';
 import './css/index.css';
-import { displayTodos } from './js/todo.js';
+import { displayTodos, addTodo } from './js/todo.js';
 
 // Using of the icon we want from fontawesome
 library.add(faRefresh, faArrowLeft);
 dom.watch();
 
 displayTodos();
+
+const form = document.querySelector('#addToDoForm');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addTodo();
+});
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,33 +1,7 @@
 const toDoList = document.querySelector('.toDoList');
 
 // My To Do list
-const toDoListsArray = [
-  {
-    description: ' Standup team meeting',
-    completed: false,
-    index: 5,
-  },
-  {
-    description: 'Program time 2',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: ' Program time 1',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: ' Morning session',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: ' Prep for your standup call',
-    completed: false,
-    index: 4,
-  },
-];
+const toDoListsArray = [];
 
 const displayTodos = () => {
   // Sort the table of todos
@@ -50,5 +24,21 @@ const displayTodos = () => {
   toDoList.innerHTML = myToDos;
 };
 
-// eslint-disable-next-line import/prefer-default-export
-export { displayTodos };
+const addTodo = () => {
+  const toDoInput = document.querySelector('#toDoInput');
+  const todo = toDoInput.value;
+
+  if (todo.trim().length !== 0) {
+    const todoId = toDoListsArray.length + 1;
+    const todoObj = {
+      description: todo,
+      completed: false,
+      index: todoId,
+    };
+    toDoListsArray.push(todoObj);
+    toDoInput.value = '';
+    displayTodos();
+  }
+};
+
+export { displayTodos, addTodo };
